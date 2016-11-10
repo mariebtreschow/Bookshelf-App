dc86e904093e66839851339ba636dbbac0dcb224
Marie Treschow <treschow.marie@gmail.com>
2016-11-10 11:25:37 +0100

    connceted to database

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const morgan = require('morgan'),
       pug = require('pug'),
-      express = require('express');
+      express = require('express'),
+      bodyParser = require('body-parser'),
+      Sequelize = require('sequelize');
 
 
-var app = express();
+var app = express(),
+    sequelize = new Sequelize('marietreschow', 'marietreschow', 'asta', { dialect: 'postgres' });
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.set('view engine', 'pug');
 
@@ -17,10 +21,18 @@ app.get('/books', (req, res) => {
    res.render('books/index');
 });
 
+app.post('/books', (req, res) => {
+   console.log(req.body);
+   res.redirect('/books');
+});
+
 app.get('/books/new', (req, res) => {
    res.render('books/new');
 });
 
-app.listen(3000, () => {
-   console.log('App is listening to port 3000...');
+sequelize.sync().then(() => {
+   console.log('Connected to database');
+   app.listen(3000, () => {
+      console.log('App is listening to port 3000...');
+   });
 });
